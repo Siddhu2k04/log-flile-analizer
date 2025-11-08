989fb029c5f96cfbea4e6ea925ae5cbd6d45bf1b
Siddhant Sonawane <siddhantsonawane2004@gmail.com>
2025-11-08 22:25:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -87,4 +87,7 @@ def show_analyze_page():
     return render_template("analyze.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
